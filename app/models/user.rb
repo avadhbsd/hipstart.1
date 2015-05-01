4dc705906e09b1a843b7f7837f55edc4e331bd05
Prelang Builder <builder@prelang.com>
2015-05-01 13:02:28 +0000

    Installed relation 'has_one' on model 'User'

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
   #->Prelang (user_login/devise)
   has_many :products
   has_many :collections
+  has_one :profile
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(provider: auth.provider, uid: auth.uid).first
 
